558a33732e46c0f6fa394409e06ad17f38daba8f
pgm-akdeniaz <akdenis.niaziev@student.arteveldehs.be>
2025-03-18 10:45:17 +0100

    Form validation + registration (autherisation)

--- src/app.js
+++ src/app.js
@@ -2,13 +2,14 @@
 import express from "express";
 import bodyParser from "body-parser";
 import expressEjsLayouts from "express-ejs-layouts";
-
 import { VIEWS_PATH, PORT } from "./consts.js";
-
+import cookieParser from "cookie-parser";
 
 // import actions from controllers
 import { contact, home, postContact } from "./controllers/PageController.js";
 import { getUsers } from "./controllers/api/UserController.js";
+import * as AuthController from "./controllers/AuthController.js";
+import AuthRegisterValidation from "./middleware/validation/AuthRegisterValidation.js";
 
 import {
   login,
@@ -17,6 +18,7 @@ import {
   postRegister,
   register,
 } from "./controllers/AuthController.js";
+import ContactValidation from "./middleware/validation/ContactValidation.js";
 
 const app = express();
 app.use(express.static("public"));
@@ -25,8 +27,10 @@ app.use(express.static("public"));
  * Import the body parser
  */
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
 
 app.use(expressEjsLayouts);
 app.set("view engine", "ejs");
@@ -38,13 +42,19 @@ app.set("views", VIEWS_PATH);
  */
 app.get("/login", login);
 app.get("/register", register);
-app.post("/register", postRegister, register);
+//app.post("/register", postRegister, register);
+app.post(
+  "/register",
+  AuthRegisterValidation,
+  AuthController.postRegister,
+  AuthController.register
+);
 app.post("/login", postLogin, login);
 app.post("/logout", logout);
 
 app.get("/", home);
 app.get("/contact", contact);
-app.post("/contact", postContact, contact);
+app.post("/contact", ContactValidation, postContact, contact);
 
 /**
  * API Routing
--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -5,6 +5,10 @@
 /**
  * Login
  */
+import { validationResult } from "express-validator";
+import Role from "../models/Role.js";
+import User from "../models/User.js";
+
 export const login = async (req, res) => {
   res.render("login", { layout: "layouts/authentication" });
 };
@@ -14,10 +18,97 @@ export const postLogin = async (req, res, next) => {};
  * Register
  */
 export const register = async (req, res) => {
-  res.render("register", { layout: "layouts/authentication" });
-};
-export const postRegister = async (req, res, next) => {};
+  // input fields
+  const inputs = [
+    {
+      name: "firstname",
+      label: "Voornaam",
+      type: "text",
+      value: req.body?.firstname ? req.body.firstname : "",
+      err: req.formErrorFields?.firstname ? req.formErrorFields.firstname : "",
+    },
+    {
+      name: "lastname",
+      label: "Achternaam",
+      type: "text",
+      value: req.body?.lastname ? req.body.lastname : "",
+      err: req.formErrorFields?.lastname ? req.formErrorFields.lastname : "",
+    },
+    {
+      name: "email",
+      label: "E-mail",
+      type: "text",
+      value: req.body?.email ? req.body.email : "",
+      err: req.formErrorFields?.email ? req.formErrorFields.email : "",
+    },
+    {
+      name: "password",
+      label: "Password",
+      type: "password",
+      value: req.body?.password ? req.body.password : "",
+      err: req.formErrorFields?.password ? req.formErrorFields.password : "",
+    },
+  ];
 
+  // get the roles
+  const roles = await Role.query();
+  const flash = req.flash || {};
+
+  // render the register page
+  res.render("register", {
+    layout: "layouts/authentication",
+    inputs,
+    roles,
+    flash,
+  });
+};
+
+export const postRegister = async (req, res, next) => {
+  try {
+    const errors = validationResult(req);
+
+    // if we have validation errors
+    if (!errors.isEmpty()) {
+      // set the form error fields
+      req.formErrorFields = {};
+      errors.array().forEach((error) => {
+        req.formErrorFields[error.path] = error.msg;
+      });
+
+      // set the flash message
+      req.flash = {
+        type: "danger",
+        message: "Er zijn fouten opgetreden",
+      };
+
+      return next();
+    } else {
+      const user = await User.query().findOne({ email: req.body.email });
+      const role = await Role.query().findOne({ id: req.body.role });
+
+      // validate if the role exists in the database
+      if (!role) {
+        req.flash = { type: "danger", message: "Deze rol bestaat niet." };
+        req.formErrorFields = { role: "Deze rol bestaat niet." };
+        return next();
+      }
+      // validate if the user already exists
+      if (user) {
+        req.flash = {
+          type: "danger",
+          message: "Dit e-mail adres is al in gebruik.",
+        };
+        req.formErrorFields = { email: "Dit e-mail adres is al in gebruik." };
+        return next();
+      }
+
+      // temp res.send
+      res.send("no errors, registrate the user");
+    }
+  } catch (e) {
+    next(e.message);
+  }
+};
 /**
  * Logout
  */
